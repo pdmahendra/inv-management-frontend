0b1ab951294a7936895ae2eb61b3a18fcd32670a
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-21 03:37:40 +0530

    register user

--- src/api/query/registerUser.js
+++ src/api/query/registerUser.js
@@ -1,14 +1,21 @@
 import axios from "../../utils/middleware";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { API } from "../index";
 
-export const registerUser = async (data) => {  
+export const registerUser = async (data) => {
   const response = await axios.post(`${API.users.register}`, data);
   return response.data;
 };
-
 export const useRegister = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: registerUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["fetchAllUsers"]);
+    },
+    onError: (error) => {
+      console.error("Registration error:", error);
+    },
   });
 };
