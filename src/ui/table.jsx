acfb6c805c117070113bc5677452bdcba7ce0613
Pratik <pratikkhatri520@gmail.com>
2024-10-10 14:33:58 +0530

    changes

--- src/ui/table.jsx
+++ src/ui/table.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export const Table= ({ children }) => (
+export const Table = ({ children }) => (
   <div className="overflow-x-auto">
     <table className="min-w-full">{children}</table>
   </div>
 );
 
-export const TableHeader= ({ children }) => (
+export const TableHeader = ({ children }) => (
   <thead className="border-b-2 text-center">{children}</thead>
 );
 
-export const TableHead = ({ className, scope, children, ...props }) => (
+export const TableHead = ({ className, scope = "", children, ...props }) => (
   <th
     className={`px-5 py-4 text-center text-xs font-medium text-bold uppercase tracking-wider ${className}`}
     scope={scope}
@@ -24,14 +24,17 @@ export const TableBody = ({ children }) => (
   <tbody className="bg-white divide-y divide-gray-300">{children}</tbody>
 );
 
-export const TableRow = ({ className, children, ...props }) => (
+export const TableRow = ({ className = "", children, ...props }) => (
   <tr className={className} {...props}>
     {children}
   </tr>
 );
 
-export const TableCell = ({ className, children, ...props }) => (
-  <td className={`px-5 py-4 text-center whitespace-nowrap text-sm text-gray-500 ${className}`} {...props}>
+export const TableCell = ({ className = "", children, ...props }) => (
+  <td
+    className={`px-5 py-4 text-center whitespace-nowrap text-sm text-gray-500 ${className}`}
+    {...props}
+  >
     {children}
   </td>
 );
