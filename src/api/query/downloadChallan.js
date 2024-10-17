81442d9e29d2a62639422702a08c4d70e406c079
Pratik <pratikkhatri520@gmail.com>
2024-10-17 21:41:01 +0530

    integrated generate pdf api

---
+++ src/api/query/downloadChallan.js
@@ -0,0 +1,20 @@
+import axios from "../../utils/middleware";
+import { API } from "../index";
+
+export const downloadPdf = async (id) => {
+  try {
+    const response = await axios.get(`${API.production.generatePdf}/${id}`, {
+      responseType: "blob",
+    });
+
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "order-details.pdf");
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  } catch (error) {
+    console.error("Error downloading PDF:", error);
+  }
+};
