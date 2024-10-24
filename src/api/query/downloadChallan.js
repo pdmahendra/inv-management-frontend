900d856a483196d24a09d6962be89e607134a9a7
Pratik <pratikkhatri520@gmail.com>
2024-10-24 10:31:10 +0530

    added generate stage api function

--- src/api/query/downloadChallan.js
+++ src/api/query/downloadChallan.js
@@ -18,3 +18,20 @@ export const downloadPdf = async (id) => {
     console.error("Error downloading PDF:", error);
   }
 };
+export const downloadStagePdf = async (id,stageId) => {
+  try {
+    const response = await axios.get(`${API.lifecycle.generatePdf}/${id}/${stageId}`, {
+      responseType: "blob",
+    });
+
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "stage-details.pdf");
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  } catch (error) {
+    console.error("Error downloading PDF:", error);
+  }
+};
