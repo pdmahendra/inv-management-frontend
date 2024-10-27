101bcf10c1e034f0faab89ab9130de524feb8524
Pratik <pratikkhatri520@gmail.com>
2024-10-27 20:47:20 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,32 @@ import Agendas from "../components/home/Agendas";
 import Alarms from "../components/home/Alarms";
 import Todays from "../components/home/Todays";
 import { useUser } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const { user } = useUser();
+  const navigate = useNavigate();
 
+  const handleProfileClick = () => {
+    navigate("/profile");
+  };
   return (
     <div className="">
       <div className="py-4 border-b flex justify-between">
         <div className="text-[2rem] max-md:mt-4 max-md:ml-16">Home</div>
         <div className="rounded-lg px-2 flex gap-4 items-center py-5 md:py-1">
-          <img src={icon} className="w-10 h-10" />
+          <img
+            src={icon}
+            className="w-10 h-10 cursor-pointer"
+            onClick={handleProfileClick}
+          />
           <div className="text-black flex flex-col justify-center">
-            <div className=" flex gap-2 font-semibold text-lg">
+            <div
+              className=" flex gap-2 font-semibold text-lg cursor-pointer"
+              onClick={handleProfileClick}
+            >
               {user?.name}{" "}
-              <span className="place-self-center mt-2">
+              <span className="place-self-center mt-2 ">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
