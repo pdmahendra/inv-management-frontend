79eaee9636f8416f6fc4fbb22bed16062b054b80
Pratik <pratikkhatri520@gmail.com>
2024-10-29 23:08:37 +0530

    added thread component on inventory page

--- src/pages/RawInventory.jsx
+++ src/pages/RawInventory.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import AccessoriesComponent from "../components/inventory/rawInventory/accessories/AccessoriesComponent";
 import RollComponent from "../components/inventory/rawInventory/roll/RollComponent";
 import AstarComponent from "../components/inventory/rawInventory/astar/AstarComponent";
-
+import ThreadComponent from "../components/inventory/rawInventory/thread/ThreadComponent";
 export default function RawInventory() {
   return (
     <div className="p-6 max-sm:space-y-8">
       <div className="max-md:mt-2 text-[2rem] flex justify-between">
         <div className="max-sm:pl-12">Raw Inventory</div>
       </div>
-      <AccessoriesComponent />
       <RollComponent />
+      <AccessoriesComponent />
+      <ThreadComponent />
       <AstarComponent />
     </div>
   );
