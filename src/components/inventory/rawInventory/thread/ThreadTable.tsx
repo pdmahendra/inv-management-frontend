751384cd216df8a2d6d13d8bf08f174591c23677
Pratik <pratikkhatri520@gmail.com>
2024-10-29 23:08:17 +0530

    added thread table

---
+++ src/components/inventory/rawInventory/thread/ThreadTable.tsx
@@ -0,0 +1,121 @@
+import * as React from "react";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../../../ui/table";
+// import EditItem from "../accessories/editItem";
+
+export const columns = [
+  {
+    accessorKey: "srNo",
+    header: "Sr No.",
+    cell: ({ row }) => <div>{row.index + 1}</div>,
+  },
+  {
+    accessorKey: "color",
+    header: "Colour",
+    cell: ({ row }) => <div className="">{row.original.color}</div>,
+  },
+  {
+    accessorKey: "no",
+    header: "Number",
+    cell: ({ row }) => <div className="pl-3">{row.original.no}</div>,
+  },
+  {
+    accessorKey: "quantity",
+    header: "Quantity",
+    cell: ({ row }) => <div className="pl-6">{row.original.quantity}</div>,
+  },
+];
+
+export default function ThreadTable({ data = [] }) {
+  // const [sorting, setSorting] = useState([]);
+  // const [columnFilters, setColumnFilters] = useState([]);
+  // const [columnVisibility, setColumnVisibility] = React.useState({});
+  // const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    // onSortingChange: setSorting,
+    // onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    // onColumnVisibilityChange: setColumnVisibility,
+
+    // onRowSelectionChange: setRowSelection,
+    // state: {
+    //   sorting,
+    //   columnFilters,
+    //   columnVisibility,
+    //   rowSelection,
+    // },
+  });
+
+  return (
+    <div className="border border-gray-300 rounded-3xl mt-6">
+      <div className="p-4">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} className="px-14">
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="px-12">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
