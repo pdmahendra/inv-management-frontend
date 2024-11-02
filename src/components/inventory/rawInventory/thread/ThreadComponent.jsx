d67d12237f59f854ae246305e26f93c5a08ed18b
Pratik <pratikkhatri520@gmail.com>
2024-11-02 11:01:15 +0530

    added thread component

--- src/components/inventory/rawInventory/thread/ThreadComponent.jsx
+++ src/components/inventory/rawInventory/thread/ThreadComponent.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
 import FormDialog from "./ThreadDialog";
 import ThreadTable from "./ThreadTable";
+import { useFetchInventoryData } from "../../../../api/query/inventory/invetoryApi";
 
-const AstarComponent = () => {
+const ThreadComponent = () => {
+  const {
+    data: inventoryData = { items: [] },
+    isLoading: isFetching,
+    refetch,
+  } = useFetchInventoryData();
+  const filteredInventoryData = inventoryData.items?.filter(
+    (item) => item.inventory_type === "raw" && item.sub_category === "thread"
+  );
 
-    
   return (
     <div className="sm:pr-16 w-[380px] sm:w-full sm:pr-16">
       <div className="flex justify-between items-center sm:pl-6 sm:pt-8">
         <h1 className="text-2xl">Thread</h1>
-        <FormDialog />
+        <FormDialog refetch={refetch} />
       </div>{" "}
-      <ThreadTable />
+      <ThreadTable data={filteredInventoryData} refetch={refetch} />
     </div>
   );
 };
 
-export default AstarComponent;
+export default ThreadComponent;
