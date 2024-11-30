1cf72aefb4fcaa0426bc5d9c341c4c1b9f15d304
Pratik <pratikkhatri520@gmail.com>
2024-11-30 15:20:00 +0530

    added notification component

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import NotificationCard from "../components/NotificationCard";
+import { useGetNotifications } from "../api/query/notificationApi";
 function Notifications() {
+  const { data } = useGetNotifications();
+
+  console.log(data);
+
   return (
     <div className="">
       <div className="p-6">
-        <div className="max-sm:mt-2 text-[2rem] flex justify-between">
+        <div className="max-sm:mt-2 text-3xl flex justify-between ">
           <div className="max-sm:pl-12">Notifications</div>
         </div>
-        <div className="mt-4">
-          <NotificationCard
-            name="Stock Limit Reached"
-            color="#FFB5B5"
-            buttonHeading={"Go to Inventory"}
-            link={"/inventory"}
-          />
+        <div className="mt-4 space-y-4">
+          {data &&
+            data.map((n) => {
+              console.log("Notification ID:", n.id);
+              return (
+                <NotificationCard
+                  message={n.message}
+                  id={n.id}
+                  type={n.type}
+                  color="#FFB5B5"
+                />
+              );
+            })}
         </div>
       </div>
     </div>
